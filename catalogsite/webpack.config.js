203ff0bbae4a9003540a61bb9bc2771ba24c2536
Yofret Rios <yofret.rios@jobsity.com>
2016-11-25 16:37:29 -0500

    Adding file loader

--- catalogsite/webpack.config.js
+++ catalogsite/webpack.config.js
@@ -1,20 +1,28 @@
 module.exports = {
-	context : __dirname + "/app", // defining application context it will app folder created at root level
-	entry   : "./scripts/app.js", // defining our main entry file, it will be app.js, look at it as the main class in java, or the first file in the execution 
+	context    : __dirname + "/app", // defining application context it will app folder created at root level
+	entry      : {
+		javascript : "./scripts/app.jsx", // defining our main entry file, it will be app.js, look at it as the main class in java, or the first file in the execution
+		html       : "./index.html"
+	},
   	
   	// resolve extensions
 	resolve: {
 		extensions: ['', '.js', '.jsx', '.json']
 	},
 
-	// configure loaders This basically transpile our ES6 code to ES5 old standard, Transform JSX to JS, the browser can understand everything
 	module: {
 		loaders: [
 			{
+				// configure loaders This basically transpile our ES6 code to ES5 old standard, Transform JSX to JS, the browser can understand everything
 				test    : /\.jsx?$/,
 				exclude : /node_modules/,
 				loaders : ["babel-loader"]
-			}
+			},
+				// loading the index file we will require only one
+			{
+			    test   : /\.html$/,
+			    loader : "file?name=[name].[ext]",
+  			}
 		]
 	},
 
