5f62a123c674d8a412a14a70d650589ce99ac5ef
Yofret Rios <yofret.rios@jobsity.com>
2016-11-25 16:51:42 -0500

    Fixing references

--- catalogsite/app/scripts/app.jsx
+++ catalogsite/app/scripts/app.jsx
@@ -3,7 +3,7 @@
 // --------------------------------------------------
 
 	import React from 'react';
-	import ReactDom from 'react-dom';
+	import ReactDOM from 'react-dom';
 	import Main from './main'
 
 // --------------------------------------------------
