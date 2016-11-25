5ef59de18828a8a848331fef7c3d5ea68d846406
Yofret Rios <yofret.rios@jobsity.com>
2016-11-25 16:03:00 -0500

    Add Babel module configuration

--- catalogsite/webpack.config.js
+++ catalogsite/webpack.config.js
@@ -1,6 +1,22 @@
 module.exports = {
 	context : __dirname + "/app", // defining application context it will app folder created at root level
 	entry   : "./scripts/app.js", // defining our main entry file, it will be app.js, look at it as the main class in java, or the first file in the execution 
+  	
+  	// resolve extensions
+	resolve: {
+		extensions: ['', '.js', '.jsx', '.json']
+	},
+
+	// configure loaders This basically transpile our ES6 code to ES5 old standard, Transform JSX to JS, the browser can understand everything
+	module: {
+		loaders: [
+			{
+				test    : /\.jsx?$/,
+				exclude : /node_modules/,
+				loaders : ["babel-loader"]
+			}
+		]
+	},
 
 	output : {  // We define our ouput file, which will be bundle.js
 		path     : __dirname + "/dist",
