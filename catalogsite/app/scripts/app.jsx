53a288a639934b1043761bbae946e0b57484da40
Yofret Rios <yofret.rios@jobsity.com>
2016-11-25 16:30:40 -0500

    Adding React First component

---
+++ catalogsite/app/scripts/app.jsx
@@ -0,0 +1,16 @@
+// --------------------------------------------------
+// Dependecies
+// --------------------------------------------------
+
+	import React from 'react';
+	import ReactDom from 'react-dom';
+	import Main from './main'
+
+// --------------------------------------------------
+// Start App Component
+// --------------------------------------------------
+
+	ReactDOM.render(
+		<Main/>, 
+		document.getElementById('app')
+	);
---
+++ catalogsite/app/scripts/main.jsx
@@ -0,0 +1,41 @@
+// --------------------------------------------------
+//  Dependencies
+// --------------------------------------------------
+
+	import React, { Component } from 'react';
+
+// --------------------------------------------------
+//  React Class Component
+// --------------------------------------------------
+	
+	class Main extends Component {
+
+		// Component Life Cycle
+
+		componentWillMount() {}
+
+		componentDidMount() {}
+
+		componentWillUpdate(nextProps, nextState) {}
+
+		componentDidUpdate(prevProps, prevState) {}
+
+		componentWillReceiveProps(nextProps) {}
+
+		// Functions
+
+		// Private Functions
+
+		// render functions
+
+		render() {
+			return (
+				<div>Hello React!!!</div>
+			);
+		}
+	}
+
+
+	export default Main
+	
+
