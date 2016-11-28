c2ec3aff642a4f17b89cf3969ef0d3e9043edd00
Yofret Rios <yofret.rios@jobsity.com>
2016-11-28 15:31:09 -0500

    Gulp + Webpack + React Boilerplate

--- catalogsite/app/scripts/app.js
+++ catalogsite/app/scripts/app.js
@@ -11,9 +11,15 @@
 	class App extends Component {
 		render() {
 			return (
-				<div>
-					<h1>Hello baby</h1>
-					<img src='images/one.jpg' />
+				<div className='wrapper'>
+					<h1 className='title'>Ready for this stuff</h1>
+					<p className='desc'>
+						Gulp + Webpack + React Boilerplate <br/>
+						is up to you whats next....
+					</p>
+					<div className='image-wrapper'>
+						<img src='images/one.jpg' />
+					</div>
 				</div>
 			);
 		}
